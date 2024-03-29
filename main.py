e5e362d451dd7babc17ef9ec195707d2c45b8163
jamesarrojo <jamesarrojo.01@gmail.com>
2024-03-29 20:34:39 +0800

    added my solution for Count words

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    word_count = count_words(text)
     print(text)
+    print(word_count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def count_words(text):
+    return len(text.split())
 
 main()
