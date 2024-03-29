c8d6147fed83f4994cb1c40fe10eb5845d144d43
jamesarrojo <jamesarrojo.01@gmail.com>
2024-03-29 23:13:23 +0800

    added my solution for 'Print a Report' and also boot.dev answer key for reference

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
-    print(num_chars)
+    num_letters = filter_letters(num_chars)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document\n")
+
+    for letter in num_letters:
+        print(f"The '{list(letter.keys())[0]}' character was found {list(letter.values())[0]} times")
+        
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,5 +30,17 @@ def get_num_chars(text):
         else:
             num_chars[char] = 1
     return num_chars
+
+def filter_letters(chars):
+    num_letters = []
+    for char in chars:
+        if char.isalpha():
+            num_letters.append({char: chars[char]})
+    def sort_on(dict):
+        return list(dict.values())[0]
+
+    num_letters.sort(reverse=True, key=sort_on)
+
+    return num_letters
 
 main()
